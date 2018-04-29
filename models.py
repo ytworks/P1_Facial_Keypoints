2935f4d874e54688cee2efd18ea9a1f6873032a2
ytworks <yuzo.takagi@gmail.com>
2018-04-29 21:50:58 +0900

    Submit

--- models.py
+++ models.py
@@ -9,29 +9,29 @@ import torch.nn.init as I
 
 
 class Net(nn.Module):
-
+    
     def __init__(self):
         super(Net, self).__init__()
+    
+        self.conv1 = nn.Conv2d(1, 8, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(44944, 68*4)
+        self.fc2 = nn.Linear(68*4, 68*2)
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        # dropout with p=0.4
+        #self.fc1_drop = nn.Dropout(p=0.4)
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
-
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
-        # a modified x, having gone through all the layers of your model, should be returned
-        return x
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = x.view(x.size(0), 68, 2)
+        return x 
+
+
